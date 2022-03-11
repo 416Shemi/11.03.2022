9186b46a2a757c04a112646eb964f48b3b0e9834
Shamsaddin Amanov <tu7ngcsvr@aztu.local>
2022-03-11 14:25:29 +0400

    new projekt

---
+++ ConsoleApp1/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Models
+{
+    class Person:Baba
+    {
+      public  string name;
+      public string surname;
+      public int age;
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ConsoleApp1.Models;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student stud = new Student();
+            Teacher tch = new Teacher();
+
+            
+        }
+    }
+}
